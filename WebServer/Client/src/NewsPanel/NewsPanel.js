ddfd389a4f49caa79dd43a1c126d8aacb78f12c4
cxxichen <chenxi920511@gmail.com>
2018-05-21 17:48:47 -0400

    complete server side code of authentication service

--- WebServer/Client/src/NewsPanel/NewsPanel.js
+++ WebServer/Client/src/NewsPanel/NewsPanel.js
@@ -2,6 +2,7 @@ import './NewsPanel.css';
 import React from 'react';
 import NewsCard from '../NewsCard/NewsCard';
 import _ from 'lodash';
+import Auth from '../Auth/Auth';
 
 class NewsPanel extends React.Component {
     constructor() {
@@ -29,6 +30,9 @@ class NewsPanel extends React.Component {
     loadMoreNews() {
         let request = new Request('http://localhost:3000/news', {
             method: 'GET',
+            headers: {
+              'Authorization': 'bearer ' + Auth.getToken(),
+            },
             cache: false
         });
 
--- WebServer/Server/app.js
+++ WebServer/Server/app.js
@@ -1,6 +1,10 @@
+var bodyParser = require('body-parser');
+var cors = require('cors');
 var express = require('express');
+var passport = require('passport');
 var path = require('path');
 
+var authRouter = require('./routes/auth');
 var indexRouter = require('./routes/index');
 var newsRouter = require('./routes/news');
 
@@ -12,14 +16,26 @@ app.set('view engine', 'jade');
 app.use('/static', express.static(path.join(__dirname, '../Client/build/static/')));
 
 // TODO: remove this after development is done
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
+app.use(cors());
+app.use(bodyParser.json());
+
+var config = require('./config/config.json');
+require('./models/main.js').connect(config.mongoDbUri);
+
+// load passport strategies
+app.use(passport.initialize());
+var localSignupStrategy = require('./passport/signup_passport');
+var localLoginStrategy = require('./passport/login_passport');
+passport.use('local-signup', localSignupStrategy);
+passport.use('local-login', localLoginStrategy);
+
+// pass the authenticaion checker middleware
+const authCheckMiddleware = require('./middleware/auth_checker');
+app.use('/news', authCheckMiddleware);
 
 app.use('/', indexRouter);
 app.use('/news', newsRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
